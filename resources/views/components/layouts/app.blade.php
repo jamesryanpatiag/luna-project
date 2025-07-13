1ade6395f1acb77e83ed006aca474cf247112d90
James Ryan Patiag <james@mindnation.com>
2025-07-14 02:33:23 +0800

    added favicon on livewire components

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/png">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
         <title>{{ $title ?? 'Page Title' }}</title>
